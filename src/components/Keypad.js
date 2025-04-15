bb53492b07e6d2bfe8547a61fd1c624bdfa47827
Iann <iconian4@gmail.com>
2025-04-15 14:30:05 +0300

    initialize commit

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,17 @@
 // Code Keypad Component Here
+import React from 'react';
 
 function Keypad (){
+    const hanldePasswordChange = (e) => {
+        console.log('Entering password...');
+    }
     return (
-        <div></div>
-    )
+        <div>
+            <input 
+            type="password"
+            onChange={hanldePasswordChange} />
+        </div>
+    )   
 }
 
 export default Keypad;
